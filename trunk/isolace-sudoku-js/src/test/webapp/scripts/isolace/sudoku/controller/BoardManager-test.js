003f8c3199018b13d50cbce2249d01705335d04c
nbasham <nbasham@0e84418e-2eb7-11de-874f-bb9810f3bc79>
2010-01-31 02:04:02 +0000

    Removing files.

--- trunk/isolace-sudoku-js/src/test/webapp/scripts/isolace/sudoku/controller/BoardManager-test.js
+++
@@ -1,7 +0,0 @@
-module("isolace/sudoku/controller/BoardManager");
-
-test("constructor", function() {
-    var bm = new ISOLACE.sudoku.BoardManager(state);
-    ok(bm, "BoardManager instanciated");
-});
-
